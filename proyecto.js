ffa482f8983d15153e38bfb78d304743aa0c2b40
Karla <karjuacas@gmail.com>
2025-11-11 18:54:11 +0100

    proyecto3

--- proyecto.js
+++ proyecto.js
@@ -1,6 +1,22 @@
-let cont = 0;
-let div = document.createElement ("div");
-let interval = setInterval(() => {
-    div.textContent = alert ('Bienvenido, pulsa Control + F10 o epera 5 segundos.');
-    document.body.appendChild(div);
-}, 5000);
+let div = document.getElementById('welcome');
+let user = document.getElementById('user');
+user.style.display = 'none';
+
+let temp = setTimeout(function () {
+    div.style.display = 'none';
+}, 5000);     
+
+document.addEventListener("keypress", function(tecla) {
+    if (tecla.key === "o" || tecla.key === "k") {
+        div.style.display = 'none';
+        show();
+    }
+})
+
+function show() {
+    user.style.display = 'block';
+}
+
+function validateEmail() {
+    if (valiName.)
+}

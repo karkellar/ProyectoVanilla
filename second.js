e3c4e69f504034075ca5a38bd5bf777762ca5bdc
Karla <karjuacas@gmail.com>
2025-11-13 20:44:41 +0100

    proyecto6

--- second.js
+++ second.js
@@ -1,2 +1,15 @@
+let greetings = document.createElement('h1');
 
-localStorage.getItem ("emailUser.value");
+greetings.innerText = '¡HOLA ' + localStorage.getItem("usuario") + '!';
+
+document.getElementById('hey').appendChild(greetings);
+
+let lastDate = localStorage.getItem('fecha');
+if (lastDate) {
+    let div = document.createElement('div');
+    div.innerText = "ÚLTIMA VEZ QUE INGRESASTE FUE: " + lastDate;
+    document.getElementById("hey").appendChild(div);
+}
+
+//Recuperamos los datos guardados en localStorage
+localStorage.setItem("fecha", new Date().toLocalString());
